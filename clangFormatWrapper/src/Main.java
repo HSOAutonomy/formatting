330c0f6098562b8eb1f081c2f4e9e1dfaac22bea
Jens Fischer <jensfischer95@gmail.com>
2017-04-01 18:35:49 +0200

    Recursively walk directories

--- clangFormatWrapper/src/Main.java
+++ clangFormatWrapper/src/Main.java
@@ -1,7 +1,13 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -32,12 +38,17 @@ public class Main
 			System.exit(1);
 		}
 
+		for (String file : args) {
+			walkFiles(file, this ::format);
+		}
+	}
+
+	private void format(File file) throws URISyntaxException, IOException
+	{
 		String executable = getJarLocation() + "/binaries/windows/clang-format.exe";
 		String style = String.format("-style={%s}", String.join(", ", STYLE));
 		String[] command = new String[] {executable, "-i", style};
-		command = Stream.concat(Stream.of(command), Stream.of(args)).toArray(String[] ::new);
-
-		System.out.println(Arrays.toString(command));
+		command = Stream.concat(Stream.of(command), Stream.of(file.getAbsolutePath())).toArray(String[] ::new);
 
 		ProcessBuilder builder = new ProcessBuilder(command);
 		builder.inheritIO();
@@ -47,5 +58,36 @@ public class Main
 	private static String getJarLocation() throws URISyntaxException
 	{
 		return new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParent();
+	}
+
+	private void walkFiles(String path, FileVisitor fileVisitor) throws IOException, URISyntaxException
+	{
+		File file = new File(path);
+
+		if (file.getParentFile().getName().equals(".git")) {
+			return;
+		}
+
+		if (!file.isDirectory()) {
+			String name = file.getName();
+			if (name.endsWith(".java") || name.endsWith(".c") || name.endsWith(".h") || name.endsWith(".cpp") ||
+				name.endsWith(".hpp")) {
+				fileVisitor.visit(file);
+				return;
+			}
+		}
+
+		File[] list = file.listFiles();
+		if (list == null)
+			return;
+
+		for (File f : list) {
+			walkFiles(f.getAbsolutePath(), fileVisitor);
+		}
+	}
+
+	@FunctionalInterface
+	interface FileVisitor {
+		void visit(File file) throws IOException, URISyntaxException;
 	}
 }
